4db6632b2110258ac2895351d46509b0da588c90
pablomacielcrypto <81847772+pablomacielcrypto@users.noreply.github.com>
2021-10-02 22:45:05 -0300

    Clase Categoria

---
+++ app/src/main/java/com/example/myapplication/Conexion/Categoria.java
@@ -0,0 +1,26 @@
+package com.example.myapplication.Conexion;
+
+public class Categoria {
+    int id_categoria;
+
+    public Categoria() {
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    String descripcion;
+
+    public int getId_categoria() {
+        return id_categoria;
+    }
+
+    public void setId_categoria(int id_categoria) {
+        this.id_categoria = id_categoria;
+    }
+}
